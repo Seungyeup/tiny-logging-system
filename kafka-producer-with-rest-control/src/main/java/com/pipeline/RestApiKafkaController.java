a036906a631d3375a4cc6e1a1c67c2ee983fcfb3
Seungyeup <lsyeup1206@naver.com>
2022-10-13 22:44:48 +0900

    troble shooting - gradle, KafkaTemplate

---
+++ kafka-producer-with-rest-control/src/main/java/com/pipeline/RestApiKafkaController.java
@@ -0,0 +1,50 @@
+package com.pipeline;
+
+import com.google.gson.Gson;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+import org.springframework.web.bind.annotation.*;
+
+import org.slf4j.Logger;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class RestApiKafkaController {
+    private final Logger logger = LoggerFactory.getLogger(RestApiKafkaProducer.class);
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public RestApiKafkaController(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @GetMapping("/api/select")
+    public void selectLog(
+        @RequestHeader("user-agent") String userAgentName,
+        @RequestParam(value = "log_type") String logType,
+        @RequestParam(value = "cus_no") String cusNo){
+        SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZZ");
+        Date now = new Date();
+        Gson gson = new Gson();
+        UserEventVO userEventVO = new UserEventVO(sdfDate.format(now), userAgentName, logType, cusNo );
+        String jsonEventLog = gson.toJson(userEventVO);
+        kafkaTemplate.send("shinhan-mydata-log", jsonEventLog).addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                logger.error(ex.getMessage(), ex);
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                logger.info(result.toString());
+            }
+        });
+    }
+}
--- kafka-producer-with-rest-control/src/main/java/com/pipeline/UserEventVO.java
+++ kafka-producer-with-rest-control/src/main/java/com/pipeline/UserEventVO.java
@@ -1,7 +1,16 @@
 package com.pipeline;
 
 public class UserEventVO {
+
+    private String timestamp;
+    private String userAgent;
+    private String cus_no;
+    private String log_type;
+
     public UserEventVO(String timestamp, String userAgent, String cus_no, String log_type){
-        
+        this.timestamp = timestamp;
+        this.timestamp = userAgent;
+        this.timestamp = cus_no;
+        this.timestamp = log_type;
     }
 }
