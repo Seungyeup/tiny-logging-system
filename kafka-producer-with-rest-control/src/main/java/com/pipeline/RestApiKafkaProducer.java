4d28bbe23bfa74490e5ee3e65b7ddef92e0dd510
Seungyeup <lsyeup1206@naver.com>
2022-10-13 16:39:14 +0900

    update log type

--- kafka-producer-with-rest-control/src/main/java/com/pipeline/RestApiKafkaProducer.java
+++ kafka-producer-with-rest-control/src/main/java/com/pipeline/RestApiKafkaProducer.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PipelineApplication {
+public class RestApiKafkaProducer {
 
     public static void main(String[] args) {
-        SpringApplication.run(PipelineApplication.class, args);
+        SpringApplication.run(RestApiKafkaProducer.class, args);
     }
 
 }
---
+++ kafka-producer-with-rest-control/src/main/java/com/pipeline/UserEventVO.java
@@ -0,0 +1,7 @@
+package com.pipeline;
+
+public class UserEventVO {
+    public UserEventVO(String timestamp, String userAgent, String cus_no, String log_type){
+        
+    }
+}
