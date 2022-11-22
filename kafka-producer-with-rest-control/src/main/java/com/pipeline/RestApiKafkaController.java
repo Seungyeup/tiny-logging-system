63811423482b40c3e8f1ee143d03e312a3a00aec
Seungyeup <lsyeup1206@naver.com>
2022-11-22 23:28:34 +0900

    kafka template

--- kafka-producer-with-rest-control/src/main/java/com/pipeline/RestApiKafkaController.java
+++ kafka-producer-with-rest-control/src/main/java/com/pipeline/RestApiKafkaController.java
@@ -2,7 +2,7 @@ package com.pipeline;
 
 import com.google.gson.Gson;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFutureCallback;
@@ -18,7 +18,7 @@ import java.util.Date;
 public class RestApiKafkaController {
     private final Logger logger = LoggerFactory.getLogger(RestApiKafkaProducer.class);
 
-    private final KafkaTemplate<String, String> kafkaTemplate; // kafka template 인스턴스를 생성. 이때 Bxm-Framework 에서는 해당 기능을 개발해야 할 수도 있다.
+    private final KafkaTemplate<String, String> kafkaTemplate; // kafka template 인스턴스를 생성.
 
     public RestApiKafkaController(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
@@ -35,7 +35,7 @@ public class RestApiKafkaController {
         UserEventVO userEventVO = new UserEventVO(sdfDate.format(now), userAgentName, logType, cusNo );
         String jsonEventLog = gson.toJson(userEventVO);
         logger.info(jsonEventLog);
-
+        // when success and fail
         kafkaTemplate.send("shinhan-mydata-log", jsonEventLog).addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
             @Override
             public void onFailure(Throwable ex) {
--- kafka-producer-with-rest-control/src/main/java/com/pipeline/UserEventVO.java
+++ kafka-producer-with-rest-control/src/main/java/com/pipeline/UserEventVO.java
@@ -2,6 +2,7 @@ package com.pipeline;
 
 public class UserEventVO {
 
+    // 시간값을 알고 있으면 컨슈머에서 병렬처리가 되더라도 최종 적재된 데이터의 timestamp 를 기준으로 정렬할 수 있다.
     private String timestamp;
     private String userAgent;
     private String cus_no;
