ea3c9076b102db6c2e0a9ed74eafa3a50d4f3a5e
Seungyeup <lsyeup1206@naver.com>
2023-01-18 23:30:50 +0900

    for kafka java 17

---
+++ kafka-simple-producer/src/main/java/com/example/kafkasimpleproducer/KafkaSimpleProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkasimpleproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSimpleProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSimpleProducerApplication.class, args);
+	}
+
+}
